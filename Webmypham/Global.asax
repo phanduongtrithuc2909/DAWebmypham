ad4acff5c2103b658ca903a07e272f627ddbf202
Trí Thức <thucphan2909@gmail.com>
2024-03-24 14:34:34 +0700

    Add project files.

---
+++ Webmypham/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Webmypham.MvcApplication" Language="C#" %>
---
+++ Webmypham/Models/Metadata/DANHMUC.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Webmypham.Models
+{
+    public class DANHMUC
+    {
+        public int IdDM { get; set; }
+        public string TenDM { get; set; }
+
+        public DANHMUC(int id, string ten)
+        {
+            IdDM = id;
+            TenDM = ten;
+        }
+    }
+}
